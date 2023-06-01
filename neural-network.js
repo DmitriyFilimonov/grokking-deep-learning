34aaf66b53351090c2bd8fc065848326484e2c9c
dmitriyf <dmitriyf@xcritical.software>
2023-06-02 01:21:44 +0300

    ???

--- neural-network.js
+++ neural-network.js
@@ -1,15 +1,15 @@
 const weighedSum = (inputs, _weights) => {
-    const output = inputs.reduce((acc, input, index) => acc += input * _weights[index], 0);
+  const output = inputs.reduce((acc, input, index) => acc += input * _weights[index], 0);
 
-    return output;
+  return output;
 }
 
-let weights = [[0.1, 0.2, 0]];
+let weights = [0.1, 0.2, 0];
 
 const getWinningProbability = (input, _weights) => {
-    const prediction = weighedSum(input, _weights);
+  const prediction = weighedSum(input, _weights);
 
-    return prediction;
+  return prediction;
 }
 
 const numberOfToes = [8.5, 9.5, 10, 9];
@@ -18,22 +18,40 @@ const fansCountBillions = [1.2, 1.3, 0.5, 1.0];
 
 const input = [numberOfToes[0], winRate[0], fansCountBillions[0]];
 
-const winningProbability = getWinningProbability(input, weights[0]);
+const winningProbability = getWinningProbability(input, weights);
 
 // ------------------------------------------------------------------------
 
 const elementWiseMulti = (n, vector) => {
-    return vector.map((vectorComponent) => n * vectorComponent);
+  return vector.map((vectorComponent) => n * vectorComponent);
 }
 
-weights.push([0.3, 0.2, 0.9]);
+const multipleInputWeights = {
+  numberOfToes: [0.1, 0.1, -0.3],
+  winRate: [0.1, 0.2, 0],
+  sadness: [0, 1.3, 0.1]
+}
 
 const getWinningSadnessAndTraumaProbabilities = (input, _weights) => {
-    prediction = elementWiseMulti(input, _weights);
+  prediction = elementWiseMulti(input, _weights);
 
-    return prediction;
+  return prediction;
 }
 
-const prediction2 =  getWinningSadnessAndTraumaProbabilities(0.65, weights[1]);
+const prediction1 = getWinningSadnessAndTraumaProbabilities(0.65, multipleInputWeights.numberOfToes);
+const prediction2 = getWinningSadnessAndTraumaProbabilities(0.65, multipleInputWeights.winRate);
+const prediction3 = getWinningSadnessAndTraumaProbabilities(0.65, multipleInputWeights.sadness);
 
-console.log(prediction2);
+const weightsMatrix = [multipleInputWeights.numberOfToes, multipleInputWeights.winRate, multipleInputWeights.sadness]
+
+const matrixMulti = (vector, matrix) => {
+  return matrix.map(matrixItem => weighedSum(vector, matrixItem))
+}
+
+const threeInputsThreeOutputs = (input, weights) => {
+  const prediction = matrixMulti(input, weights);
+
+  return prediction;
+}
+
+console.log({ threeInputsThreeOutputs: threeInputsThreeOutputs(input, weightsMatrix) });
