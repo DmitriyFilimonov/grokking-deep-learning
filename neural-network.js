4be70b5b2438e1d278d7a37a9ba4b3fdb8f8ed9c
dmitriyf <dmitriyf@xcritical.software>
2023-06-01 21:19:44 +0300

    47: weighedSum function is implemented

--- neural-network.js
+++ neural-network.js
@@ -1,15 +1,23 @@
-let weight = 0.1;
+const weighedSum = (inputs, _weights) => {
+    const output = inputs.reduce((acc, input, index) => acc += input * _weights[index], 0);
 
-const neuralNetwork = (input, weight) => {
-    const prediction = input * weight;
+    return output;
+}
+
+let weights = [0.1, 0.2, 0];
+
+const neuralNetwork = (input, _weights) => {
+    const prediction = weighedSum(input, _weights);
 
     return prediction;
 }
 
 const numberOfToes = [8.5, 9.5, 10, 9];
+const winRate = [0.65, 0.8, 0.8, 0.9];
+const fansCountBillions = [1.2, 1.3, 0.5, 1.0];
 
-const input = numberOfToes [0];
+const input = [numberOfToes[0], winRate[0], fansCountBillions[0]];
 
-const prediction = neuralNetwork(input, weight);
+const prediction = neuralNetwork(input, weights);
 
 console.log(prediction);
