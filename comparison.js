c5e74463eec71b05df6bdbe8428bdbadbe4ddde2
dmitriyf <dmitriyf@xcritical.software>
2023-06-10 21:03:21 +0300

    gradient

--- comparison.js
+++ comparison.js
@@ -4,33 +4,17 @@ const neuralNetwork = (input, weight) => {
     return prediction;
 }
 
-let weight = 0.5;
+let weight = 100;
+const goal_target = 41.365;
 const input = 0.5;
-const goal_target = 0.8;
 
-const step = 0.001;
-
-for (let i = 0; i < 1101; i++) {
+for (let i = 0; i < 30; i++) {
     const prediction = neuralNetwork(input, weight);
-    const error = Math.pow(prediction - goal_target, 2);
+    const pureError = prediction - goal_target
+    const errorDirectionAndAmount = pureError * input;
+    weight -= errorDirectionAndAmount
 
-    console.log(`Error: ${error}. Prediction: ${prediction}`);
-
-    const increasedWeight = weight + step;
-    const increasedWeightPrediction = neuralNetwork(input, increasedWeight);
-    const increasedWeightError = Math.pow(increasedWeightPrediction - goal_target, 2);
-
-    const decreasedWeight = weight - step;
-    const decreasedWeightPrediction = neuralNetwork(input, decreasedWeight);
-    const decreasedWeightError = Math.pow(decreasedWeightPrediction - goal_target, 2);
-
-    if (decreasedWeightError < increasedWeightError) {
-        weight = decreasedWeight;
-    }
-
-    if (decreasedWeightError > increasedWeightError) {
-        weight = increasedWeight;
-    }
+    console.log(`Prediction: ${prediction}. Pure error: ${pureError} Weight: ${weight}.`);
 }
 
 
